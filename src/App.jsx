b8cc1ce27cb1766635aacc759e065a3dd733f8e3
cagatay.yildiz <cagatay@pixupplay.com>
2024-11-11 14:58:37 +0300

    Add React Router for navigation; implement Home, About, Contact, and NotFound pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import Categories from "./propSample/Categories";
-import { categoriesData } from "./data/categoriesData";
-import ProductBox from "./propSample/ProductBox";
-
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 function App() {
 
 
+
   return <>
-    <Categories categoriesData={categoriesData} />
-    <ProductBox title="Product 1" price={100} image="https://via.placeholder.com/150" description="Description 1" points={["point 1", "point 2", "point 3"]} hello={() => alert("Hello")} />
+    <nav>
+      <ul style={{display:'flex', justifyContent:'space-between'}}>
+        <li><Link to={'/'}>Home</Link></li>
+        <li><Link to={'/about'}>About</Link></li>
+        <li><Link to={'/contact'}>Contact</Link></li>
+      </ul>
+    </nav>
+    <hr />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <hr />
+    <h1>Site Footer</h1>
   </>
 }
 
