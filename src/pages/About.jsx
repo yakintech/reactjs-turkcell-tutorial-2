45c185453dce42ef5741c18f7e277ac3b0f19788
cagatay.yildiz <cagatay@pixupplay.com>
2024-11-12 15:42:14 +0300

    Add SWR for data fetching; implement category fetching in About component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,27 @@
+import axios from 'axios'
 import React from 'react'
+import useSWR from 'swr'
 
 function About() {
-  return (
+
+  const { data, isLoading } = useSWR("categories", async () => {
+    return axios.get("https://northwind.vercel.app/api/categories")
+  }, {refreshInterval: 5000})
+
+  console.log("data", data)
+
+  return <>
     <h1>About</h1>
-  )
+    {
+      isLoading ? <h1>Loading...</h1> : <ul>
+        {
+          data.data.map(category => (
+            <li key={category.id}>{category.name}</li>
+          ))
+        }
+      </ul>
+    }
+  </>
 }
 
 export default About
