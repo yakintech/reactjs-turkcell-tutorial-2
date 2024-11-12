ecba6535585622f7c57b9ea1a59058c0e20f6fe2
cagatay.yildiz <cagatay@pixupplay.com>
2024-11-12 15:48:26 +0300

    Add React Query for data fetching; implement category fetching in Contact component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
 
+const queryClient = new QueryClient();
+
+root.render(
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </QueryClientProvider>
 );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,29 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import axios from 'axios'
 
 function Contact() {
-  return (
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['categories'],
+    queryFn: async () => {
+      return axios.get("https://northwind.vercel.app/api/categories")
+    }
+  })
+
+  return <>
     <h1>Contact</h1>
-  )
+    <hr />
+    {
+      isLoading ? <h1>Loading...</h1> : <ul>
+        {
+          data.data.map(category => (
+            <li key={category.id}>{category.name}</li>
+          ))
+        }
+      </ul>
+    }
+  </>
 }
 
 export default Contact
