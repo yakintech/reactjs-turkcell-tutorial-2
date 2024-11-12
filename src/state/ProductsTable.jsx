a6f6de74449f22d36212207a25f65e887631cf43
cagatay.yildiz <cagatay@pixupplay.com>
2024-11-12 11:49:41 +0300

    Add search functionality to ProductsTable; implement product filtering by name and price range

--- src/state/ProductsTable.jsx
+++ src/state/ProductsTable.jsx
@@ -4,8 +4,48 @@ import { productsData } from '../data/productsData'
 function ProductsTable() {
 
     const [products, setproducts] = useState(productsData)
+    const [minPrice, setminPrice] = useState(0)
+    const [maxPrice, setmaxPrice] = useState(0)
+
+
+
+    const searchProduct = (e) => {
+        var name = e.target.value
+        var filteredProducts = productsData.filter(item => item.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()))
+        setproducts(filteredProducts)
+    }
+
+    const search = () => {
+        let min = Number(minPrice)
+        let max = Number(maxPrice)
+
+        if (!isNaN(min) && !isNaN(max)) {
+            var filteredProducts = productsData.filter(item => item.unitPrice >= min && item.unitPrice <= max)
+            setproducts(filteredProducts)
+        }
+        if(min == 0 && max == 0){
+            setproducts(productsData)
+        }
+
+    }
 
     return <>
+        <div>
+            <label htmlFor="">Search:</label>
+            <input type='text' onChange={searchProduct} />
+        </div>
+        <hr />
+        <div>
+            <label htmlFor="">Min Price</label>
+            <input type='number' onChange={(e) => setminPrice(e.target.value)} />
+            <label htmlFor="">Max Price</label>
+            <input type='number' onChange={(e) => setmaxPrice(e.target.value)} />
+            <button onClick={search}>Search</button>
+        </div>
+
+        <hr />
+        <p>Length: {products.length}</p>
+        <hr />
         <table className='w3-table w3-striped'>
             <thead>
                 <tr>
